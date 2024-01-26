4674cdd7e966f2c9fc06c4bdcb56899df253ca5d
Aamleen Ahmed <aamleen20002@iiitd.ac.in>
2024-01-26 20:18:37 +0530

    Added encryption and decryption functions. Added function to verify the decrypted message

--- Assignment 1/2020312_2020002.py
+++ Assignment 1/2020312_2020002.py
@@ -31,13 +31,68 @@ def generate_key():
             key += digit
     return key
 
+def generate_new_plain_text(plain_text, key_length, hash_length=256):
+    '''Adds the required garbage characters to the plaintext for filling the transposition matrix.
+    new_plain_text = plain_text + garbage_characters'''
+    characters_present = len(plain_text) + hash_length + 1      # +1 for the "#" character
+    garbage_characters = key_length - characters_present % key_length
 
-def encryption(plain_text, key):
-    pass
+    new_plain_text = plain_text + "."*garbage_characters
+    return new_plain_text
+
+def transposition_matrix(new_plain_text, key):
+    '''Returns the transposition matrix of the new_plain_text'''
+    matrix = []
+    n_cols = len(key)
+    n_rows = len(new_plain_text) // n_cols
+
+    for i in range(n_rows):
+        matrix.append(list(new_plain_text[i*n_cols:(i+1)*n_cols]))
+
+    return matrix
+
+def encrypt(plain_text):
+    '''Calculates the hash value of the new_plain_text and appends it to the new_plain_text.
+    Encrypts the message using the key and returns the cipher_text
+    to_be_encrypted = plain_text + garbage_characters + "#" + hash_value'''
+
+    key = generate_key()
+    new_plain_text = generate_new_plain_text(plain_text, len(key))
+    hash_value = generate_hash_value(new_plain_text)
+
+    to_be_encrypted = new_plain_text + "#" + hash_value
+    cipher_text = ""
+
+    matrix = transposition_matrix(to_be_encrypted, key)
+    for i in range(len(key)):
+        for j in range(len(matrix)):
+            cipher_text += matrix[j][int(key[i])]
+
+    return cipher_text, key
 
 
-def decryption(cipher_text, key):
-    pass
+def decrypt(cipher_text, key):
+    '''Decrypts the cipher_text using the key and returns the decrypted_text.
+    Removes the hash value from the decrypted_text and returns it.'''
+
+    decrypted_text = ""
+    matrix = transposition_matrix(cipher_text, key)
+    for i in range(len(key)):
+        for j in range(len(matrix)):
+            decrypted_text += matrix[j][key.index(str(i))]
+
+    split_index = decrypted_text.rfind("#")
+    hash_value = decrypted_text[split_index+1:]
+    decrypted_text = decrypted_text[:split_index]
+
+    return decrypted_text, hash_value
+
+
+def verify_decrypted_text(decrypted_text, hash_value):
+    '''Returns True if the hash value of the decrypted_text matches the hash_value passed as argument.
+    Returns False otherwise.'''
+
+    return generate_hash_value(decrypted_text) == hash_value
 
 
 def brute_force_attack(cipher_text, hash_value, key_len):
@@ -45,7 +100,7 @@ def brute_force_attack(cipher_text, hash_value, key_len):
     while True:
         # Assumption: hash value is always appended at the end of the plaintext. (There can be extra characters after plaintext and before hash value. Need to remove them.)
         guess_key = "0" * (key_len - len(guess_key)) + guess_key
-        decrypted_text = decryption(cipher_text, guess_key)
+        decrypted_text = decrypt(cipher_text, guess_key)
         decrypted_hash_value = decrypted_text[-len(hash_value):]
 
         if decrypted_hash_value == hash_value:
